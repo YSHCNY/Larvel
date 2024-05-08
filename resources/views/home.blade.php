19d1cb96d625bcbf33c40c1bbd26bedc3efaa3e7
Yoshiyuki Canoy <ycanoy@PHILKOEI.COM.PH>
2024-05-08 16:24:17 +0800

    added tailwind

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="resources\css\main.css" rel="stylesheet">
+    
+    <title></title>
+    
+</head>
+<body>
+
+<center>
+<h1 class = 'text-[red]'>Add Product</h1>
+
+<form>
+    <div>
+        <label for="title">Product Title</label>
+        <input type="text" name = "title">
+    </div>
+
+    <div>
+        <label for="description">Product Description</label>
+        <textarea name="description" id=""></textarea>
+    </div>
+
+
+    <div>
+        <label for="image">Product Image</label>
+        <input type="file" name = "image">
+    </div>
+
+
+    <div>
+    
+        <input type="submit" name = "Add Product">
+    </div>
+</form>
+
+</center>
+    
+</body>
+</html>
