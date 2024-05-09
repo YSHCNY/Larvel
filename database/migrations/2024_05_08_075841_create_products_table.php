d478646e69ee0d073d994885f24128a7240d4865
Yoshiyuki Canoy <ycanoy@PHILKOEI.COM.PH>
2024-05-09 16:24:43 +0800

    details

--- database/migrations/2024_05_08_075841_create_products_table.php
+++ database/migrations/2024_05_08_075841_create_products_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->integer('code')->nullable();
             
+            $table->text('address')->nullable();
             $table->string('title')->nullable();
 
             $table->text('description')->nullable();
