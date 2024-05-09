dfafef65c704e6869b6ba8843f91df233dfd295b
Yoshiyuki Canoy <ycanoy@PHILKOEI.COM.PH>
2024-05-09 11:11:31 +0800

    added daisy ui and fixed framework

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,41 +3,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="resources\css\main.css" rel="stylesheet">
+   
     
-    <title></title>
-    
+    <title>Laravel Crud</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
-<center>
-<h1 class = 'text-[red]'>Add Product</h1>
+<div class = ' container flex mx-auto '>
+    <div class="shadow p-9 m-4  mx-auto w-auto justify-center items-center ">
+<p class = 'font-bold mb-0'>Add Product</p>
+<p class = ' italic text-gray-400 text-sm'>Crud practice for laravel</p>
 
-<form>
+<div class = ' mt-4'>
+<form class = ''>
     <div>
-        <label for="title">Product Title</label>
-        <input type="text" name = "title">
+    <label class="form-control w-full max-w-xs">
+  <div class="label">
+    <span class="label-text">Product Name</span>
+   
+  </div>
+  <input type="text" name = 'title' placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+</label>
     </div>
 
     <div>
-        <label for="description">Product Description</label>
-        <textarea name="description" id=""></textarea>
-    </div>
+    <label class="form-control">
+  <div class="label">
+    <span class="label-text">Product Description</span>
+ 
+  </div>
+  <textarea name = 'description' class="textarea textarea-bordered h-24" placeholder="Type here..."></textarea>
+</label>
+</div>
 
 
     <div>
-        <label for="image">Product Image</label>
-        <input type="file" name = "image">
+    <label class="form-control w-full max-w-xs">
+  <div class="label">
+    <span class="label-text">Pick a file</span>
+
+  </div>
+  <input type="file" name = 'image' class="file-input file-input-bordered w-full max-w-xs" />
+
+</label>
     </div>
 
 
-    <div>
+    <div class = 'float-end my-5'>
     
-        <input type="submit" name = "Add Product">
+    <button type = 'submit' class="btn btn-neutral">Add</button>
     </div>
 </form>
+</div>
 
-</center>
+
+</div>
+</div>
+
     
 </body>
 </html>
